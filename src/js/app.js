857c5e13e6d076488d42f065efcc821b705a161b
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-05-25 23:32:13 -0300

    Finished part 1

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+const parallax_el = document.querySelectorAll(".parallax");
+
+let xValue = 0, 
+    yValue = 0;
+
+window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    let zValue = 100;
+
+    parallax_el.forEach((el) => {
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
+    });
+});
